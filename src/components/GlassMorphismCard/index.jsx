a484c9d796314dac8237db0f72e82a0e54611760
zealotProtoss1 <zealot@protoss.exchange>
2023-04-15 22:45:18 +0900

    change card ui detail

--- src/components/GlassMorphismCard/index.jsx
+++ src/components/GlassMorphismCard/index.jsx
@@ -7,12 +7,12 @@ const GlassmorphismCard = ({imgSrc, repoUrl, description}) => {
       className='card-container'
       onClick={() => window.open(repoUrl, '_blank')}
       style={{
-        backgroundImage: `url(${imgSrc})`,
+        // backgroundImage: `url(${imgSrc})`,
       }}
     >
       <div className='card-wrapper'>
         <div className='card-content'>
-          {/*<img alt='badge' src={imgSrc} width={512} height={512} />*/}
+          <img className='badge' alt='badge' src={imgSrc} width={256} height={256} />
           <p>{description}</p>
           <div onClick={() => window.open(repoUrl, '_blank')} className={'more-btn'}>Read More</div>
         </div>
